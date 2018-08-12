e4e9cc69d99c65055b3f608eef49d491cdf730c1
Zac Bell <zchrbell7@gmail.com>
2018-08-11 18:53:40 -0600

    v2

--- word-frequency.py
+++ word-frequency.py
@@ -9,8 +9,20 @@ def wordFrequency(sentence):
     print "Word Frequency...."
     for key in frequencyDictionary:
         print "the word", key, "occurred", frequencyDictionary[key], "times"
-    #print frequencyDictionary
-	#print frequencyDictionary["One"]
     return frequencyDictionary
 
 wordFrequency("One Two Three Three Three Four Four Four Four Five Six")
+
+
+"""
+---Output---
+
+Word Frequency....
+the word Four occurred 4 times
+the word Six occurred 1 times
+the word Five occurred 1 times
+the word One occurred 1 times
+the word Three occurred 3 times
+the word Two occurred 1 times
+
+"""
