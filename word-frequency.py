af53a6ddcdd315635c39922bfc95a5abe7927682
Zac Bell <zchrbell7@gmail.com>
2018-08-14 18:53:51 -0600

    v3

--- word-frequency.py
+++ word-frequency.py
@@ -1,28 +1,38 @@
+import re
+
 def wordFrequency(sentence):
     frequencyDictionary = {}
-    words = sentence.split()
-    for key in words:
-        if key in frequencyDictionary:
-            frequencyDictionary[key] += 1
-        else:
-            frequencyDictionary.update({key:1})
-    print "Word Frequency...."
-    for key in frequencyDictionary:
-        print "the word", key, "occurred", frequencyDictionary[key], "times"
-    return frequencyDictionary
 
-wordFrequency("One Two Three Three Three Four Four Four Four Five Six")
+    sentence = re.sub(r'[^\w\s]', ' ', sentence)
+    # [ ^ ...] Matches any single character not in brackets
+    # \w Matches word characters.
+    # \s Matches whitespace.
+
+    words = sentence.lower().split()
+    # take the sentence string, make all characters lowercase, and then split into words
+
+    for word in words:
+        if word in frequencyDictionary:
+            frequencyDictionary[word] += 1
+        else:
+            frequencyDictionary.update({word:1})
+    print "Word Frequency...."
+    for entry in frequencyDictionary:
+        print "The word", entry, "occurred", frequencyDictionary[entry], "times."
+    return frequencyDictionary
+    # end word frequency function
+
+wordFrequency("Exclamation! exclamation hyphen-hyphen Three Three--Four Four 'Four' four; Five SIX")
 
 
 """
 ---Output---
 
 Word Frequency....
-the word Four occurred 4 times
-the word Six occurred 1 times
-the word Five occurred 1 times
-the word One occurred 1 times
-the word Three occurred 3 times
-the word Two occurred 1 times
-
+The word exclamation occurred 2 times.
+The word four occurred 4 times.
+The word five occurred 1 times.
+The word hyphen occurred 2 times.
+The word six occurred 1 times.
+The word three occurred 2 times.
 """
